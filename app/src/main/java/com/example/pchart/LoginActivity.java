c5739abc23ec091b9f5fe91ae5f78d9595e6ae26
Elly Wenani <ellywenani@gmail.com>
2020-07-29 03:37:27 +0300

    Made changes on activity_login.xml and the other code generaly to improve performance

--- app/src/main/java/com/example/pchart/LoginActivity.java
+++ app/src/main/java/com/example/pchart/LoginActivity.java
@@ -134,10 +134,10 @@ public class LoginActivity extends AppCompatActivity {
                     //check if email is verified
                     if (user.isEmailVerified()) {
                         Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
-                        Toast.makeText(LoginActivity.this, "Authenticated with: " + user.getEmail(), Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getApplicationContext(), "Authenticated with: " + user.getEmail(), Toast.LENGTH_SHORT).show();
 
                         Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        //intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                         startActivity(intent);
                         finish();
 
