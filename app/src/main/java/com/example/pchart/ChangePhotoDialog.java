073bf5526f25f9ed1380c4878ef73d11c02d585d
Elly Wenani <ellywenani@gmail.com>
2020-08-11 21:38:14 +0300

    Created ChangePhotoDialog.class and coded its xml

---
+++ app/src/main/java/com/example/pchart/ChangePhotoDialog.java
@@ -0,0 +1,14 @@
+package com.example.pchart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChangePhotoDialog extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_photo_dialog);
+    }
+}
